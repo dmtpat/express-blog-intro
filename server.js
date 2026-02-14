edd9850cf80da11470f82915858926a3974a73f0
Pat <dmtpatricica@email.com>
2026-02-14 12:56:49 +0100

    image + static  path

--- server.js
+++ server.js
@@ -8,34 +8,39 @@ const posts = [
     {
         titolo: "pippo",
         contenuto: "lorem ipsum",
-        image: "#"
+        image: "http://localhost:3000/image1.jpg"
     },
     {
         titolo: "mario",
         contenuto: "lorem ipsum",
-        image: "#"
+        image: "/image2.jpg"
     },
     {
         titolo: "mariella",
         contenuto: "lorem ipsum",
-        image: "#"
+        image: "/image3.jpg"
     },
     {
         titolo: "gisella",
         contenuto: "lorem ipsum",
-        image: "#"
+        image: "/image4.jpg"
     },
     {
         titolo: "peppe",
         contenuto: "lorem ipsum",
-        image: "#"
+        image: "/image5.jpg"
     }
 ]
 
-// app.use();
+app.use(express.static('public'));
 
 //app.get(rotta , funzione)
 app.get('/', (req, res) => {
+    console.log("Ecco la prima chiamata!");
+    res.send("Server del mio blog");// invia una risposta alla richiesta per la rotta di base '/'
+    //res.json(posts);
+})
+app.get('/bacheca/', (req, res) => {
     console.log("Ecco la prima chiamata!");
     //res.send("Server del mio blog");// invia una risposta alla richiesta per la rotta di base '/'
     res.json(posts);
